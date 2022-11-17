db0a2a0cdfafaefd728ba2702f7788e0616a9bef
Kyle McCormick <kdmc@pm.me>
2022-11-17 18:06:11 -0500

    iterating

--- tutorkdmccormick/configdiff.py
+++ tutorkdmccormick/configdiff.py
@@ -11,15 +11,17 @@ def _record_tutor_root(root: str):
     @click.command()
     def configdiff():
         script = f"""\
-set -xeuo pipefail
+set -euo pipefail
 rm -rf /tmp/tutor-configdiff
 mkdir -p /tmp/tutor-configdiff
-mv {root}/env /tmp/tutor-configdiff/env-save
-tutor config save
-diff {root}/env /tmp/tutor-configdiff/env-save
-mv /tmp/tutor-configdiff/env-save {root}/env
+cd /tmp/tutor-configdiff
+mv {root}/env ./CURRENT_ENV
+tutor config save 1>/dev/null
+mv {root}/env ./NEW_ENV
+diff CURRENT_ENV NEW_ENV && echo "Your Tutor environment is up to date." || true
+mv ./CURRENT_ENV {root}/env
 """
-        #subprocess.check_call(["echo", script])
+        subprocess.check_call(["echo", script])
         subprocess.check_call(["bash", "-c", script])
 
 
