d390f2937d8f5d289bc456d8c331690d16b71f7e
Kyle McCormick <kdmc@pm.me>
2022-11-21 15:11:26 -0500

    iterating

--- tutorkdmccormick/configdiff.py
+++ tutorkdmccormick/configdiff.py
@@ -1,7 +1,8 @@
 import typing as t
+from subprocess import check_call
+from pathlib import PurePath
 
 import click
-import subprocess
 from tutor import hooks
 
 
@@ -10,19 +11,27 @@ def _record_tutor_root(root: str):
 
     @click.command()
     def configdiff():
+        tmp_area = PurePath("/") / 'tmp' / 'tutor' / 'configdiff'
+        compare_root = tmp_area
+        actual_root = PurePath(root)
+        before = PurePath('BEFORE')
+        after = PurePath('AFTER')
         script = f"""\
 set -euo pipefail
-rm -rf /tmp/tutor-configdiff
-mkdir -p /tmp/tutor-configdiff
-cd /tmp/tutor-configdiff
-mv {root}/env ./CURRENT_ENV
-tutor config save 1>/dev/null
-mv {root}/env ./NEW_ENV
-diff CURRENT_ENV NEW_ENV && echo "Your Tutor environment is up to date." || true
-mv ./CURRENT_ENV {root}/env
+rm -rf '{tmp_area}'
+mkdir -p '{tmp_area / after}'
+ln -s '{actual_root}' '{tmp_area / before}'
+cp '{actual_root / "config.yml"}' '{tmp_area / after}'
+TUTOR_ROOT='{tmp_area / after}' tutor config save 1>/dev/null
+cd {tmp_area}
+diff '{before}/env' '{after}/env' && echo "Your Tutor environment is up to date." || true
 """
-        subprocess.check_call(["echo", script])
-        subprocess.check_call(["bash", "-c", script])
+        check_call(["rm", "-rf", compare_root])
+        check_call(["mkdir", "-p", compare_root / after])
+        check_call(["ln", "-s", actual_root, compare_root / before])
+        check_call(["bash", "-c", f"TUTOR_ROOT='{tmp_area / after}' tutor config save 1>/dev/null"])
+        check_call(["bash", "-c", f"cd '{tmp_area}' && diff '{before}' '{after}'"])
 
+        #check_call(["bash", "-c", script])
 
     hooks.Filters.CLI_COMMANDS.add_item(configdiff)
